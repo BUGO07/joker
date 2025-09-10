fb569612725500e949dc6ad11071e2ab18cb28be
BUGO07 <71205861+BUGO07@users.noreply.github.com>
2025-09-11 01:49:49 +0400

    deal cards

---
+++ src/card.rs
@@ -0,0 +1,20 @@
+pub struct Card(String);
+
+impl Card {
+    pub fn value(&self) -> usize {
+        if self.0.starts_with("Joker") {
+            return 15;
+        }
+        if self.0.ends_with("10") {
+            return 10;
+        }
+
+        match &self.0[1..2] {
+            "A" => 14,
+            "K" => 13,
+            "Q" => 12,
+            "J" => 11,
+            x => x.parse().unwrap(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use bevy::prelude::*;
+use bevy::{platform::collections::HashMap, prelude::*};
+
+pub mod card;
 
 fn main() {
     App::new()
+        .init_resource::<CardAssets>()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Joker Game".to_string(),
@@ -13,6 +16,44 @@ fn main() {
         .run();
 }
 
-fn setup() {
-    println!("joker rahh")
+#[derive(Resource, Default)]
+pub struct CardAssets(HashMap<String, Handle<Image>>);
+
+fn setup(mut commands: Commands, mut assets: ResMut<CardAssets>, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+
+    for asset in std::fs::read_dir("assets/cards").unwrap() {
+        if let Ok(entry) = asset
+            && let Ok(file_type) = entry.file_type()
+            && file_type.is_file()
+        {
+            let name = entry
+                .path()
+                .file_stem()
+                .unwrap()
+                .to_string_lossy()
+                .into_owned();
+
+            assets.0.insert(
+                name,
+                asset_server.load(format!(
+                    "cards/{}",
+                    entry.file_name().to_string_lossy().into_owned()
+                )),
+            );
+        }
+    }
+
+    let mut deck = assets.0.keys().cloned().collect::<Vec<_>>();
+
+    let mut players = HashMap::new();
+    for name in ["lela", "ilia", "lizi", "giorgi"] {
+        players.insert(name, Vec::new());
+    }
+    for cards in players.values_mut() {
+        for _ in 0..9 {
+            let card = deck.remove(rand::random_range(0..deck.len()));
+            cards.push(card);
+        }
+    }
 }
