d238f1cf36edde42a743485bf71d2a0d38a6626e
BUGO07 <71205861+BUGO07@users.noreply.github.com>
2025-09-11 13:06:10 +0400

    picking, wasm support, bugfix

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use bevy::{
     prelude::*,
     window::{PrimaryWindow, WindowResized},
 };
+use random_number::random;
+// use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 
 use crate::card::Card;
 
@@ -21,6 +23,7 @@ fn main() {
             }),
             ..Default::default()
         }))
+        // .add_plugins((EguiPlugin::default(), WorldInspectorPlugin::default()))
         .add_systems(Startup, (load_assets, start_game).chain())
         .add_systems(
             Update,
@@ -44,6 +47,44 @@ const CARD_HEIGHT: f32 = 400.0;
 const CARD_SCALE: f32 = 1.0 / 5.0;
 const CSW: f32 = CARD_WIDTH * CARD_SCALE;
 const CSH: f32 = CARD_HEIGHT * CARD_SCALE;
+const ASSETS: &[&str] = &[
+    "JokerRed",
+    "JokerBlack",
+    "S7",
+    "S8",
+    "S9",
+    "S10",
+    "SJ",
+    "SQ",
+    "SK",
+    "SA",
+    "D6",
+    "D7",
+    "D8",
+    "D9",
+    "D10",
+    "DJ",
+    "DQ",
+    "DK",
+    "DA",
+    "C7",
+    "C8",
+    "C9",
+    "C10",
+    "CJ",
+    "CQ",
+    "CK",
+    "CA",
+    "H6",
+    "H7",
+    "H8",
+    "H9",
+    "H10",
+    "HJ",
+    "HQ",
+    "HK",
+    "HA",
+];
 
 fn load_assets(
     mut commands: Commands,
@@ -51,27 +92,11 @@ fn load_assets(
     asset_server: Res<AssetServer>,
 ) {
     commands.spawn(Camera2d);
-
-    for asset in std::fs::read_dir("assets/cards").unwrap() {
-        if let Ok(entry) = asset
-            && let Ok(file_type) = entry.file_type()
-            && file_type.is_file()
-        {
-            let name = entry
-                .path()
-                .file_stem()
-                .unwrap()
-                .to_string_lossy()
-                .into_owned();
-
-            assets.0.insert(
-                name,
-                asset_server.load(format!(
-                    "cards/{}",
-                    entry.file_name().to_string_lossy().into_owned()
-                )),
-            );
-        }
+    for name in ASSETS.iter() {
+        assets.0.insert(
+            name.to_string(),
+            asset_server.load(format!("cards/{}.png", name)),
+        );
     }
 }
 
@@ -92,29 +117,32 @@ fn start_game(
     for name in ["lela", "ilia", "lizi", "giorgi"] {
         players.0.push_back((name.to_string(), Vec::new()));
     }
-    for (i, (_player, cards)) in players.0.iter_mut().enumerate() {
-        for _ in 0..9 {
-            let card = deck.remove(rand::random_range(0..deck.len()));
-            cards.push(Card(card, i));
-        }
-        cards.sort_by_key(|x| std::cmp::Reverse(x.value()));
-        cards.sort_by_key(|x| x.suit());
-    }
 
     let current_player = "giorgi";
     while players.0[0].0 != current_player {
         let player = players.0.pop_front().unwrap();
         players.0.push_back(player)
     }
+
+    for (i, (_player, cards)) in players.0.iter_mut().enumerate() {
+        for _ in 0..9 {
+            let card = deck.remove(random!(..deck.len()));
+            cards.push(Card(card, i));
+        }
+        cards.sort_by_key(|x| std::cmp::Reverse(x.value()));
+        cards.sort_by_key(|x| x.suit());
+    }
     let covered_card = asset_server.load("back/Red.png");
 
     for (i, (player, cards)) in players.0.iter().enumerate() {
-        let mut player_node = commands.spawn((
-            Visibility::Visible,
-            Transform::from_rotation(Quat::from_rotation_z(i as f32 * -90f32.to_radians())),
-            PlayerNode,
-        ));
-        player_node.with_child((
+        let player_node = commands
+            .spawn((
+                Visibility::Visible,
+                Transform::from_rotation(Quat::from_rotation_z(i as f32 * -90f32.to_radians())),
+                PlayerNode,
+            ))
+            .id();
+        commands.spawn((
             Text2d::new(player),
             Transform::from_xyz(
                 0.0,
@@ -127,31 +155,43 @@ fn start_game(
                 0.0,
             )
             .with_rotation(Quat::from_rotation_z(i as f32 * 90f32.to_radians())),
+            ChildOf(player_node),
         ));
 
         for (j, card) in cards.iter().enumerate() {
-            player_node.with_child((
-                Sprite::from_image(if player == current_player {
-                    assets.0[&card.0].clone()
-                } else {
-                    covered_card.clone()
-                }),
-                Transform::from_translation(if i % 2 == 0 {
-                    vec3(
-                        (j as f32 - (9.0 - 1.0) / 2.0) * CSW,
-                        (-window.height() + CSH + 25.0) / 2.0,
-                        0.0,
-                    )
-                } else {
-                    vec3(
-                        (j as f32 - (9.0 - 1.0) / 2.0) * CSW,
-                        (-window.width() + CSW + 25.0) / 2.0,
-                        0.0,
-                    )
-                })
-                .with_scale(Vec3::ONE * CARD_SCALE),
-                card.clone(),
-            ));
+            commands
+                .spawn((
+                    Pickable::default(),
+                    Sprite::from_image(if player == current_player {
+                        assets.0[&card.0].clone()
+                    } else {
+                        covered_card.clone()
+                    }),
+                    Transform::from_translation(if i % 2 == 0 {
+                        vec3(
+                            (j as f32 - (9.0 - 1.0) / 2.0) * CSW,
+                            (-window.height() + CSH + 25.0) / 2.0,
+                            0.0,
+                        )
+                    } else {
+                        vec3(
+                            (j as f32 - (9.0 - 1.0) / 2.0) * CSW,
+                            (-window.width() + CSW + 25.0) / 2.0,
+                            0.0,
+                        )
+                    })
+                    .with_scale(Vec3::ONE * CARD_SCALE),
+                    card.clone(),
+                    ChildOf(player_node),
+                ))
+                .observe(
+                    |trigger: Trigger<Pointer<Released>>,
+                     cards: Query<&Card>,
+                     players: Res<Players>| {
+                        let card = cards.get(trigger.target).unwrap();
+                        info!("card - {} | player - {}", card.0, players.0[card.1].0)
+                    },
+                );
         }
     }
 }
@@ -164,7 +204,7 @@ fn resize_event(
 ) {
     for event in resize_event.read() {
         for (mut transform, card) in cards.iter_mut() {
-            transform.translation.y = if (3 - card.1) % 2 == 0 {
+            transform.translation.y = if card.1 % 2 == 0 {
                 (-event.height + CSH + 25.0) / 2.0
             } else {
                 (-event.width + CSW + 25.0) / 2.0
